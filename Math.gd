82a6c69a1e823d9e2fee7c012b769dced13151c1
lampperson <jamesevora367@gmail.com>
2020-09-20 00:00:25 -0700

    commit_9/20/2020
    player character feels really good to control now

---
+++ Math.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+func interpolate_angle(a, b, amount):
+	var difference_one = b - a
+	var wrap_difference = a if a < 180 else 360 - a
+	var difference_two = wrap_difference
+	difference_two += 360 - b if a < 180 else b
+	
+	var difference
+	var dir
+	if abs(difference_one) < difference_two:
+		difference = abs(difference_one)
+		dir = sign(difference_one)
+	else:
+		difference = difference_two
+		dir = -1 if a < 180 else 1
+	
+	if difference < amount:
+		return b
+	else:
+		return a + (amount*dir)
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,77 +1,110 @@
 extends KinematicBody
 
+#mouse sensitivity
 var sensitivity = 0.1
+
+# rotation
+onready var target_pirate_y_rotation = $Pirate.rotation_degrees.y
+onready var pirate_y_rotation = $Pirate.rotation_degrees.y
+
+# movement
 var movement_speed = 8
 var acceleration = 5
-var air_acceleration = 2
 var jump_force = 30
 var run_multiplier = 3
+var rotation_speed = 10
+var velocity = Vector3()
+
+var air_movement_speed = 12
+var air_acceleration = 2
+var air_velocity = Vector3()
 
 # animations
-var animation_playing = ["", false]
-var set_animation = ["Idle", false]
+var animation_playing = ""
+var set_animation = "Idle-loop"
 
-var velocity = Vector3()
+func set_blend_times():
+	var blend_time = 0.2
+	var anim_player = $Pirate/AnimationPlayer
+	var animations = ["Idle-loop", "Walk-loop", "Run-loop", "Jump-loop", "Fall-loop"]
+	for animation in animations:
+		for second_animation in animations:
+			if animation != second_animation:
+				anim_player.set_blend_time(animation, second_animation, blend_time)
+
+
 
 func _ready():
 	#locks the mouse in the same position so it doesn't touch the edges of the screen
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	$Pirate/AnimationPlayer.get_animation("Idle").set_loop(true)
-	$Pirate/AnimationPlayer.get_animation("Walk").set_loop(true)
-	$Pirate/AnimationPlayer.get_animation("Run").set_loop(true)
+	
+	#adds to player group
 	add_to_group("Player")
+	
+	#sets the blend times for animations
+	set_blend_times()
 
 func _input(event):
 	# third person camera controls
 	if event is InputEventMouseMotion:
-		rotation_degrees.y -= event.relative.x*sensitivity
-		$CameraPivot.rotation_degrees.x -= event.relative.y*sensitivity
-		$CameraPivot.rotation_degrees.x = clamp($CameraPivot.rotation_degrees.x, -50, 50)
+		$CameraPivotH.rotation_degrees.y -= event.relative.x*sensitivity
+		$CameraPivotH/CameraPivotV.rotation_degrees.x -= event.relative.y*sensitivity
+		$CameraPivotH/CameraPivotV.rotation_degrees.x = clamp($CameraPivotH/CameraPivotV.rotation_degrees.x, -50, 50)
 
 func _process(delta):
 	if animation_playing != set_animation:
-		if set_animation[1]:
-			$Pirate/AnimationPlayer.play_backwards(set_animation[0])
-		else:
-			$Pirate/AnimationPlayer.play(set_animation[0])
+		$Pirate/AnimationPlayer.play(set_animation)
 		animation_playing = set_animation
+		
+	if target_pirate_y_rotation != pirate_y_rotation:
+		pirate_y_rotation = Math.interpolate_angle(pirate_y_rotation, target_pirate_y_rotation, rotation_speed)
+		$Pirate.rotation_degrees.y = pirate_y_rotation
 
 func _physics_process(delta):
 	movement(delta)
 
 func movement(delta):
-	set_animation = ["Idle", false]
+	set_animation = "Idle-loop"
 	var direction = Vector3()
 	
 	# setting the movement direction
+	var pivot_basis = $CameraPivotH.transform.basis
 	if Input.is_action_pressed("move_backwards"):
-		direction += transform.basis.z
-		set_animation[1] = true
+		direction += pivot_basis.z
 	if Input.is_action_pressed("move_forward"):
-		direction -= transform.basis.z
-		set_animation[1] = false	
+		direction -= pivot_basis.z
 	if Input.is_action_pressed("move_right"):
-		direction += transform.basis.x	
+		direction += pivot_basis.x	
 	if Input.is_action_pressed("move_left"):
-		direction -= transform.basis.x
+		direction -= pivot_basis.x
 	
-	if(direction != Vector3()):
-		set_animation[0] = "Walk"
-	
-	#sprint
-	var mult = 1
-	if Input.is_action_pressed("sprint") && is_on_floor():
-		mult = run_multiplier
-		if(set_animation[0] == "Walk"):
-			set_animation[0] = "Run"
-		
 	direction = direction.normalized()
-	var acceleration_using = acceleration if is_on_floor() else air_acceleration
-	velocity = velocity.linear_interpolate(direction*movement_speed*mult, acceleration_using*delta*mult)
+	if direction != Vector3():
+		target_pirate_y_rotation = rad2deg(Vector2(direction.x, -direction.z).angle()) + 90
+	
+	if is_on_floor():
+		air_velocity = Vector3()
+		
+		if direction != Vector3():
+			set_animation = "Walk-loop"
+			
+		#sprint
+		var mult = 1
+		if Input.is_action_pressed("sprint"):
+			mult = run_multiplier
+			if(set_animation == "Walk-loop"):
+				set_animation = "Run-loop"
+			
+		velocity = velocity.linear_interpolate(direction*movement_speed*mult, acceleration*delta*mult)
+		#jump
+		if Input.is_action_just_pressed("jump"):
+			velocity.y += jump_force
+	else:
+		set_animation = "Jump-loop" if velocity.y > 0 else "Fall-loop"
+		air_velocity = air_velocity.linear_interpolate(direction*air_movement_speed, air_acceleration*delta)
+		
 	#gravity
 	velocity.y += Constants.GRAVITY
-	#jump
-	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y += jump_force
-	
+	move_and_slide(air_velocity, Vector3.UP)
 	velocity = move_and_slide(velocity, Vector3.UP)
+	
