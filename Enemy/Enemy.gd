b7aac3cf2214f70cce43a934d8c2e60bedc4add0
Grazurra <65730132+Grazurra@users.noreply.github.com>
2020-09-19 20:23:43 -0400

    9/19/20

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -3,22 +3,44 @@ extends KinematicBody
 enum {
 	IDLE,
 	ALERT,
+	STUNNED
 }
 
 var state = IDLE
 
 onready var raycast = $RayCast
+#onready var anim = $enemyanimationthing?
 
+func _ready():
+	add_to_group("units")
+
+func _process(delta):
+	
+	if raycast.is_colliding():
+		state = ALERT
+	else:
+		state = IDLE
+		
+	match state:
+		IDLE:
+#			var from = project_ray_origin(event.position)
+#			var to = from + project_ray_normal(event.position) * ray_length
+#			var space_state = get_world().direct_space_state
+#			var result = space_state.intersect_ray(from, to, [], 1)
+#			if result:
+			pass
+		ALERT:
+			pass
+		STUNNED:
+			pass
+
+#Pathfinding
 var path = []
 var path_node = 0
 var speed = 10
 
 onready var nav = get_parent()
 onready var destination
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	add_to_group("units")
 
 func _physics_process(delta):
 	if path_node < path.size():
@@ -35,3 +57,10 @@ func move_to(target_pos):
 func _on_Timer_timeout():
 	move_to(destination.global_transform.origin)
 	pass
+
+
+func _on_Area_body_entered(body):
+	print(body.name + "entered")
+
+func _on_Area_body_exited(body):
+	print(body.name + ' exited')
