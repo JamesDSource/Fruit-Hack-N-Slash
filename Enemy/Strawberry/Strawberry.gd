add0234db8dd5078346696f5316b284c09ab383a
Grazurra <65730132+Grazurra@users.noreply.github.com>
2020-09-23 21:45:43 -0400

    9/23/20

--- Enemy/Strawberry/Strawberry.gd
+++ Enemy/Strawberry/Strawberry.gd
@@ -3,11 +3,14 @@ extends "res://Enemy/Enemy.gd"
 onready var anim = $Strawberry/AnimationPlayer
 var set_animation = ""
 var current_animation = ""
+var damage = 1
+var attack_array = []
 
 func update():
 	if set_animation != current_animation:
 		anim.play(set_animation)
 		current_animation = set_animation
+		attack_array = []
 	if state == ENEMYSTATE.ATTACK:
 		set_animation = "Attack"
 	elif path_node < path.size() && can_move:
@@ -16,6 +19,12 @@ func update():
 		set_animation = "Idle-loop"
 
 func attack_state():
+	if !$Strawberry/SwingHitBox/CollisionShape.disabled:
+				var bodies_hit = $Strawberry/SwingHitBox.get_overlapping_bodies()
+				for body in bodies_hit:
+					if body.is_in_group("Player") && attack_array.find(body) == -1:
+						body.damage(damage)
+						attack_array.append(body)
 	if !anim.is_playing():
 		state = ENEMYSTATE.ALERT
 		$AttackTimer.start()
